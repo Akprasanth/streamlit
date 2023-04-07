4ad5e5235298555cff0d9d6491ea15fb8cf5b4e6
Akprasanth <akrishnaprasanth@gmail.com>
2023-04-07 16:01:52 +0530

    add files via upload

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+#For Excel File
+df = pd.read_excel('C://Users//anprasanth//Downloads//Data_SDG_India_Index_2020-21.xlsx')
+st.title('World Happiness Index 2021:')
+st.write(df)
