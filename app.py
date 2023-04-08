0d3da6aed69c3795a5c9b9dad7039de4e6014853
Akprasanth <akrishnaprasanth@gmail.com>
2023-04-08 08:13:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ import plotly.express as px
 import seaborn as sns
 import matplotlib.pyplot as plt
 #For Excel File
-df = pd.read_excel('Data_SDG_India_Index_2020-21.xlsx')
-st.title('World Happiness Index 2021:')
-st.write(df)
 
 ##Filter Sidebars
 state_list = df["State/UT"].to_list()
@@ -14,10 +11,13 @@ select = st.sidebar.selectbox('Filter the state/ut here:', state_list, key='1')
 if select =="All":
     filtered_df = df
 else:
-    filtered_df = df[df["Stat/UT"]==select]
-
+    filtered_df = df[df["State/UT"]==select]
+    
+df = pd.read_excel('Data_SDG_India_Index_2020-21.xlsx')
+st.title('World Happiness Index 2021:')
+st.write(filtered_df)
 
 fig1 = plt.figure()
-ax = sns.countplot(x='SDG 2', data= df, palette = 'hls')
+ax = sns.countplot(x='SDG 2', data=filtered_df, palette = 'hls')
 st.pyplot(fig1)
 
