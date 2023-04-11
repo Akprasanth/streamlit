e9a1231dc5433f12a8bb965f75aa907a51d57b47
Akprasanth <akrishnaprasanth@gmail.com>
2023-04-11 15:57:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ import matplotlib.pyplot as plt
 #For Excel File
 df = pd.read_excel('Data_SDG_India_Index_2020-21.xlsx')
 
+#Second Excel file
+df2 = pd.read_excel('Test_file.xlsx')
+countries = tuple(df.COUNTRY.to_list())
+cat_counts = {
+    'CAT1': df.CAT1.to_numpy(),
+    'CAT2': df.CAT2.to_numpy(),
+    'CAT3': df.CAT3.to_numpy(),
+    'CAT4': df.CAT4.to_numpy(),
+}
+
 ##Filter Sidebars
 state_list = df["State/UT"].to_list()
 select = st.sidebar.selectbox('Filter the state/ut here:', state_list, key='1')
@@ -21,10 +31,17 @@ with tab1:
     col1.subheader('World Happiness Index 2021:')
     col1.write(filtered_df)
 
-    fig1 = plt.figure()
-    ax = sns.countplot(x='SDG 2', data=filtered_df, palette = 'hls')
-    col2.subheader('Plot-2')
-    col2.pyplot(fig1)
+    width = 0.8  # the width of the bars: can also be len(x) sequence
+    fig, ax = plt.subplots()
+    bottom = np.zeros(6)
+    for cat, cat_count in cat_counts.items():
+        p = ax.bar(countries, cat_count, width, label=cat, bottom=bottom)
+        bottom += cat_count
+
+        ax.bar_label(p, label_type='center')
+    ax.set_title('Countries and Status')
+    ax.legend()
+    col2.pyplot(fig)
 
     st.write(df)
 
